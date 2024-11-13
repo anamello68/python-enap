a434d0dc240c1673bf88dbd2b948e6246bade718
Ana Mello <anamello68@gmail.com>
2024-11-13 11:23:46 -0300

    Update m3-exercicio6.py

--- m3-exercicio6.py
+++ m3-exercicio6.py
@@ -4,6 +4,5 @@ import streamlit as st
 url = 'https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv'
 
 df = pd.read_csv(url)
-df.head()
 
 st.title('Localização das comunidades quilombolas (2022)')  
