10c36e41bd3b77865826902c7c6903b3830790e6
Ana Mello <anamello68@gmail.com>
2024-11-14 11:27:17 -0300

    Update m3-estudocaso2.py

--- m3-estudocaso2.py
+++ m3-estudocaso2.py
@@ -48,6 +48,35 @@ estados_siglas = {
     'TOCANTINS': 'TO'
 }
 
+estados_siglas2 = {
+    'Acre': 'AC',
+    'Alagoas': 'AL',
+    'Amapá': 'AP',
+    'Amazonas': 'AM',
+    'Bahia': 'BA',
+    'Ceará': 'CE',
+    'Espírito Santo': 'ES',
+    'Goiás': 'GO',
+    'Maranhão': 'MA',
+    'Mato Grosso': 'MT',
+    'Mato Grosso do Sul': 'MS',
+    'Minas Gerais': 'MG',
+    'Pará': 'PA',
+    'Paraíba': 'PB',
+    'Paraná': 'PR',
+    'Pernambuco': 'PE',
+    'Piauí': 'PI',
+    'Rio de Janeiro': 'RJ',
+    'Rio Grande do Norte': 'RN',
+    'Rio Grande do Sul': 'RS',
+    'Rondônia': 'RO',
+    'Roraima': 'RR',
+    'Santa Catarina': 'SC',
+    'São Paulo': 'SP',
+    'Sergipe': 'SE',
+    'Tocantins': 'TO'
+}
+
 # Criação da nova coluna 'Sigla_UF' usando o dicionário
 df['SIGLA_UF'] = df['NM_UF'].map(estados_siglas)
 
