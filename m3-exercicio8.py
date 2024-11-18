d7927e3b8affc415dbe4bb1852f6ef230e0f18c0
Ana Mello <anamello68@gmail.com>
2024-11-18 12:03:06 -0300

    Create m3-exercicio8.py

---
+++ m3-exercicio8.py
@@ -0,0 +1,25 @@
+import requests
+import pandas as pd
+import streamlit as st
+
+url = 'https://dadosabertos.camara.leg.br/api/v2/deputados?ordem=ASC&ordenarPor=nome'
+resposta = requests.get(url)
+dados = resposta.json()
+df = pd.DataFrame(dados['dados'])
+st.write("Tabela completa de deputados")
+st.write(df)
+
+# Criar uma coluna que combina 'id' e 'nome'
+df['id_nome'] = df['id'].astype(str) + ' - ' + df['nome']
+
+# Usar o selectbox para mostrar a coluna 'id_nome'
+selected = st.selectbox('Selecione um ID', df['id_nome'])
+
+# Extrair o id a partir da seleção
+selected_id = selected.split(' - ')[0]
+
+# Exibir o id selecionado
+st.write(f'ID Selecionado: {selected_id}')
+
+dadosFiltrados = df[df['id'] == selected_id]
+st.write(dadosFiltrados)
